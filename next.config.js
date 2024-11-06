5348186f4a220f8bb9b9c240535fbe6aa9c26e7f
Mrutunjay Yadav <42071203+Mrutunjay113@users.noreply.github.com>
2024-11-07 05:17:09 +0530

    Refactor Checkout components for improved readability and consistency

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io'],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'utfs.io',
-    //     port: ''
-    //   }
-    // ]
-  }
-}
+    domains: ["utfs.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
